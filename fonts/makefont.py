a7a513150cd14f1b56ab826661e541b80f6866a1
hsfzxjy <hsfzxjy@gmail.com>
2020-10-23 22:00:25 +0800

    Site updated: 2020-10-23 22:00:24

--- fonts/makefont.py
+++ fonts/makefont.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import string
 import argparse
 import tempfile
 import subprocess
@@ -11,7 +12,7 @@ WORK_DIR = ROOT_DIR
 
 
 def unicode_to_hex(char):
-    return 'U+' + hex(ord(char))[2:].upper().zfill(4)
+    return "U+" + hex(ord(char))[2:].upper().zfill(4)
 
 
 def execute(args):
@@ -19,28 +20,51 @@ def execute(args):
     p.wait()
 
 
+def get_text(args):
+    assert args.text or args.alphabet
+    if args.text:
+        return args.text
+
+    return string.ascii_letters + string.punctuation + string.digits
+
+
+def get_font_list(args):
+    for font_path in args.font:
+        font_path = Path(font_path)
+        if font_path.name.startswith("slim."):
+            continue
+
+        yield font_path
+
+
+def output_path(font_path: Path):
+    return font_path.parent / ("slim." + font_path.name)
+
+
 def main(args):
     os.chdir(WORK_DIR)
 
-    with tempfile.NamedTemporaryFile('w') as f:
-        f.writelines(map(unicode_to_hex, args.TEXT))
+    with tempfile.NamedTemporaryFile("w") as f:
+        f.writelines(map(unicode_to_hex, get_text(args)))
         f.flush()
 
-        print(f'Subsetting...')
-        execute(
-            [
-                'pyftsubset',
-                f'--unicodes-file={f.name}',
-                args.font,
-                f'--output-file=slim.{args.font}',
-            ]
-        )
+        for font_path in get_font_list(args):
+            print(f"Subsetting {font_path}...")
+            execute(
+                [
+                    "pyftsubset",
+                    f"--unicodes-file={f.name}",
+                    str(font_path),
+                    f"--output-file={output_path(font_path)}",
+                ]
+            )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('TEXT')
-    parser.add_argument('--font', default='sawarabi-mincho-medium.ttf')
+    parser.add_argument("--text", default="")
+    parser.add_argument("--font", default=["sawarabi-mincho-medium.ttf"], nargs="+")
+    parser.add_argument("--alphabet", action="store_true")
     args = parser.parse_args()
 
     main(args)
